358437a7573ad7201c869cb00837c6f39c85b56d
AlexHoop140 <alexhoop.pro@gmail.com>
2022-10-29 15:02:38 +0700

    database file

--- main.py
+++ main.py
@@ -1,33 +1,39 @@
-import mariadb
+import mysql.connector
 import sys
 
 try:
-    conn = mariadb.connect(
+    conn = mysql.connector.connect(
         user="root",
-        password="trungpro123",
-        host="localhost",
-        port=3306,
-        database="testing"
+        password="FzrTscd0aGODkVIUXtsa",
+        host="containers-us-west-44.railway.app",
+        port=5960,
+        database="railway"
     )
-except mariadb.Error as e:
-    print(f"Error connecting to MariaDB Platform: {e}")
+
+    cur = conn.cursor()
+except mysql.connector.Error as e:
+    print(f"Error connecting to Mysql Platform: {e}")
+    conn.rollback()
     sys.exit(1)
 
-cur = conn.cursor()
-# cur.execute(
-#     "CREATE TABLE sinhvien (mssv int, hoten varchar(10))"
-#     )
-cur.execute(
-    "select * from sinhvien"
-    )
-
-for mssv, hoten in cur:
-    print(f"mssv: {mssv}, hoten: {hoten}")
-
-cur.execute(
-    "show databases"
-    )
+cur.execute("SELECT * FROM sinhvien")
+# cur.execute("USES testing")
+# cur.execute("CREATE TABLE TEST("
+#             "ID NUMBER PRIMARY KEY )")
+#
+# users = cur.fetchall()
 for x in cur:
     print(x)
 
-cur.start_trans
+
+# Insert new plane information
+# def insert_plane_info(cur, reg_number, plane_name, qty_seat, qty_seat1, qty_seat2, manufacturer):
+#     cur.execute("INSERT INTO PLANE VALUES(?, ?, ?, ?, ?, ?)",
+#                 (reg_number, plane_name, qty_seat, qty_seat1, qty_seat2, manufacturer,))
+
+
+# Update plane information with registration number
+# def update_plane_info(cur, reg_number, plane_name, qty_seat, qty_seat1, qty_seat2, manufacturer):
+#     cur.execute(
+#         "UPDATE PLANE SET plane_name=?, qty_seat=?, qty_seat1=?, qty_seat2=?, manufacturer=? WHERE reg_number=?",
+#         (plane_name, qty_seat, qty_seat1, qty_seat2, manufacturer, reg_number,))
