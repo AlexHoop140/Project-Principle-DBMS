da5a8f5fe61fe0bb86d7774ad3a3eeb039a12ef6
Ngu Cong Khanh <88470967+Ceekay1607@users.noreply.github.com>
2022-11-16 22:08:29 +0700

    add new func delete_plane, show_plane_infor, show_flight_infor, delete_flight_infor

--- main.py
+++ main.py
@@ -48,6 +48,8 @@ def update_plane_info(cur, reg_number, plane_name, qty_seat, qty_seat1, qty_seat
 # insert_plane_info(cur, "VN409", "B78X", 207, 16, 191, "BOEING")
 # insert_plane_info(cur, "VN248", "B77W", 427, 42, 385, "BOEING")
 # insert_plane_info(cur, "VN301", "A359", 305, 63, 242, "AIRBUS")
+#insert a test plane to test delete function
+#insert_plane_info(cur, "CK001", "0", 0, 0, 0, "0")
 
 # Update plane info
 # update_plane_info(cur, "VN6221", "A321", 206, 16, 190, "AIRBUS")
@@ -65,3 +67,39 @@ def insert_flight_info(cur, flight_id, plane_id, departure_date, arrrival_date, 
 # insert_flight_info(cur, "S092911", "VN248", "2022-09-25", "2022-09-25", "SGN", "HAN", "")
 # insert_flight_info(cur, "T901292", "VN6221", "2022-10-24", "2022-10-24", "VCT", "DAN", "")
 # insert_flight_info(cur, "S129323", "VN409", "2022-12-23", "2022-12-24", "HAN", "LAX", "")
+#insert a test plane to test delete function
+#insert_flight_info(cur, "K999999", "CK001", "2022-12-23", "2022-12-24", "HAN", "LAX", "")
+
+
+def delete_plane(cur, plane_id):
+    cur.execute("DELETE FROM PLANES "
+                "WHERE PLANEID=%s", (plane_id,))
+    conn.commit()
+
+#test delete_plane()
+#delete_plane(cur,"CK001")
+
+def show_plane_infor(cur, plane_id):
+    cur.execute("SELECT * FROM PLANES WHERE PLANEID=%s", (plane_id,))
+    myresult = cur.fetchall()
+    for x in myresult:
+          print(x)
+#test show_plane_infor()
+#show_plane_infor(cur,"VJ305")
+
+def show_flight_infor(cur, flight_id):
+    cur.execute("SELECT * FROM FLIGHT "
+                "WHERE FLIGHTID=%s", (flight_id,))
+    myresult = cur.fetchall()
+    for x in myresult:
+        print(x)
+#test show_flight_infor()
+#show_flight_infor(cur,"S092911")
+
+def delete_flight_infor(cur, flight_id):
+    cur.execute("DELETE FROM FLIGHT WHERE FLIGHTID=%s", (flight_id,))
+    conn.commit()
+
+#test delete_flight_infor
+#delete_flight_infor(cur, "K999999")
+
