bd68d4621b8e2e4b2f6eae3848291f39437393ad
Ngu Cong Khanh <88470967+Ceekay1607@users.noreply.github.com>
2022-11-24 10:01:50 +0700

    Plane Management GUI

--- FlightGUI.py
+++ FlightGUI.py
@@ -38,66 +38,6 @@ except mysql.connector.Error as e:
     conn.rollback()
     sys.exit(1)
 
-def btn_clicked():
-    global root
-    # Plane textbox
-    root = Tk()
-    root.title('DBMS Project')
-    root.geometry("400x600")
-    global p_id
-    global p_name
-    global p_total_seat
-    global p_seat1
-    global p_seat2
-    global p_manu
-    global p_selected
-    p_id = Entry(root, width=30)
-    p_id.grid(row=0, column=1, padx=20)
-    p_name = Entry(root, width=30)
-    p_name.grid(row=1, column=1, padx=20)
-    p_total_seat = Entry(root, width=30)
-    p_total_seat.grid(row=2, column=1, padx=20)
-    p_seat1 = Entry(root, width=30)
-    p_seat1.grid(row=3, column=1, padx=20)
-    p_seat2 = Entry(root, width=30)
-    p_seat2.grid(row=4, column=1, padx=20)
-    p_manu = Entry(root, width=30)
-    p_manu.grid(row=5, column=1, padx=20)
-    p_selected = Entry(root, width=30)
-    p_selected.grid(row=8, column=1, padx=20)
-
-    # Plane insert textbox label
-    plane_id_label = Label(root, text="Plane ID")
-    plane_id_label.grid(row=0, column=0)
-    plane_name_label = Label(root, text="Plane Name")
-    plane_name_label.grid(row=1, column=0)
-    plane_total_seat_label = Label(root, text="Total Seat")
-    plane_total_seat_label.grid(row=2, column=0)
-    plane_t1_seat_label = Label(root, text="Type 1 seat number")
-    plane_t1_seat_label.grid(row=3, column=0)
-    plane_t2_seat_label = Label(root, text="Type 2 seat number")
-    plane_t2_seat_label.grid(row=4, column=0)
-    plane_manuf_label = Label(root, text="Manufacturer")
-    plane_manuf_label.grid(row=5, column=0)
-    plane_selected_label = Label(root, text="Enter Plane ID")
-    plane_selected_label.grid(row=8, column=0)
-
-    # Plane submit button
-    p_submit_btn = Button(root, text="Add New Plane To Database", command=plane_submit)
-    p_submit_btn.grid(row=6, column=0, columnspan=2, padx=10, pady=10, ipadx=100)
-    # Plane show button
-    p_show_btn = Button(root, text="Show all Planes information", command=plane_show)
-    p_show_btn.grid(row=7, column=0, columnspan=2, padx=10, pady=10, ipadx=100)
-
-    # Plane Delete button
-    p_delete_btn = Button(root, text="Delete Plane", command=plane_delete)
-    p_delete_btn.grid(row=9, column=0, columnspan=2, padx=10, pady=10, ipadx=137)
-
-    # Plane Update button
-    p_delete_btn = Button(root, text="Update Plane", command=edit)
-    p_delete_btn.grid(row=11, column=0, columnspan=2, padx=10, pady=10, ipadx=137)
-
-
 #Plane submit function
 def plane_submit():
     #create database connection
@@ -113,24 +53,24 @@ def plane_submit():
     # Insert into table
     cur.execute("INSERT INTO PLANES VALUES(%s, %s, %s, %s, %s, %s)",
                 (
-                    p_id.get(),
-                    p_name.get(),
-                    p_total_seat.get(),
-                    p_seat1.get(),
-                    p_seat2.get(),
-                    p_manu.get()
+                    plane_id_entry.get(),
+                    plane_name_entry.get(),
+                    plane_totalseat_entry.get(),
+                    plane_seat1_entry.get(),
+                    plane_seat1_entry.get(),
+                    plane_manu_entry.get()
                 ))
 
     conn.commit()
     conn.close()
 
     #clear current type in content
-    p_id.delete(0, END)
-    p_name.delete(0, END)
-    p_total_seat.delete(0, END)
-    p_seat1.delete(0, END)
-    p_seat2.delete(0, END)
-    p_manu.delete(0, END)
+    plane_id_entry.delete(0, END)
+    plane_name_entry.delete(0, END)
+    plane_totalseat_entry.delete(0, END)
+    plane_seat1_entry.delete(0, END)
+    plane_seat1_entry.delete(0, END)
+    plane_manu_entry.delete(0, END)
 
 def plane_show():
     conn = mysql.connector.connect(
@@ -143,6 +83,7 @@ def plane_show():
     cur = conn.cursor()
 
     # Show database
+    plane_show = Toplevel(window)
     cur.execute("SELECT PLANEID, PLANENAME, TOTALSEAT, TYPE1SEAT,TYPE2SEAT, MANUFACTURER FROM PLANES")
     records = cur.fetchall()
 
@@ -150,7 +91,7 @@ def plane_show():
     for record in records:
         print_record += str(record) + "\n"
 
-    show_label = Label(root, text=print_record)
+    show_label = Label(plane_show, text=print_record)
     show_label.grid(row=10, column=0, columnspan=2)
 
     conn.commit()
@@ -167,9 +108,9 @@ def plane_delete():
     cur = conn.cursor()
 
     #delete
-    cur.execute("DELETE from PLANES WHERE PLANEID=" + "\'" +(p_selected.get()) + "\'")
+    cur.execute("DELETE from PLANES WHERE PLANEID=" + "\'" +(plane_edit_entry.get()) + "\'")
 
-    p_selected.delete(0, END)
+    plane_edit_entry.delete(0, END)
 
     conn.commit()
     conn.close()
@@ -191,7 +132,7 @@ def update():
                 "TYPE1SEAT = %s, "
                 "TYPE2SEAT = %s,"
                 "MANUFACTURER = %s "
-                "WHERE PLANEID=" + "\'" +(p_selected.get()) + "\'",
+                "WHERE PLANEID=" + "\'" +(plane_edit_entry.get()) + "\'",
                 (
                     p_id_editor.get(),
                     p_name_editor.get(),
@@ -220,7 +161,7 @@ def edit():
     cur = conn.cursor()
 
     #Edit plane
-    cur.execute("SELECT * FROM PLANES WHERE PLANEID=" + "\'" +(p_selected.get()) + "\'")
+    cur.execute("SELECT * FROM PLANES WHERE PLANEID=" + "\'" +(plane_edit_entry.get()) + "\'")
     records = cur.fetchall()
 
     #define global var
@@ -280,12 +221,218 @@ background = canvas.create_image(
     808.0, 229.0,
     image=background_img)
 
+def plane_management_click():
+    plane_window = Toplevel(window)
+
+    plane_window.geometry("600x900")
+    plane_window.configure(bg="#9ba4b4")
+    plane_window.title('Plane Management')
+    plane_canvas = Canvas(
+        plane_window,
+        bg="#9ba4b4",
+        height=900,
+        width=600,
+        bd=0,
+        highlightthickness=0,
+        relief="ridge")
+    plane_canvas.place(x=0, y=0)
+    global plane_background_img
+
+    plane_background_img = PhotoImage(file=f"img/Plane/background.png")
+    plane_canvas.create_image(
+        165.0, 423.5,
+        image=plane_background_img)
+
+    global plane_id_entry
+    global plane_name_entry
+    global plane_totalseat_entry
+    global plane_seat1_entry
+    global plane_seat2_entry
+    global plane_manu_entry
+
+
+    plane_id_entry_img = PhotoImage(file=f"img/Plane/img_textBox0.png")
+    plane_id_entry_bg = plane_canvas.create_image(
+        290.0, 91.5,
+        image=plane_id_entry_img)
+
+    plane_id_entry = Entry(
+        plane_window,
+        bd=0,
+        bg="#d9d9d9",
+        highlightthickness=0)
+
+    plane_id_entry.place(
+        x=82, y=73,
+        width=416,
+        height=35)
+
+    plane_name_entry_img = PhotoImage(file=f"img/Plane/img_textBox1.png")
+    plane_name_entry_bg = plane_canvas.create_image(
+        290.0, 169.5,
+        image=plane_name_entry_img)
+
+    plane_name_entry = Entry(
+        plane_window,
+        bd=0,
+        bg="#d9d9d9",
+        highlightthickness=0)
+
+    plane_name_entry.place(
+        x=82, y=151,
+        width=416,
+        height=35)
+
+    plane_totalseat_entry_img = PhotoImage(file=f"img/Plane/img_textBox2.png")
+    plane_totalseat_entry_bg = plane_canvas.create_image(
+        291.0, 246.5,
+        image=plane_totalseat_entry_img)
+
+    plane_totalseat_entry = Entry(
+        plane_window,
+        bd=0,
+        bg="#d9d9d9",
+        highlightthickness=0)
+
+    plane_totalseat_entry.place(
+        x=83, y=228,
+        width=416,
+        height=35)
+
+    plane_seat1_entry_img = PhotoImage(file=f"img/Plane/img_textBox3.png")
+    plane_seat1_entry_bg = plane_canvas.create_image(
+        176.5, 323.5,
+        image=plane_seat1_entry_img)
+
+    plane_seat1_entry = Entry(
+        plane_window,
+        bd=0,
+        bg="#d9d9d9",
+        highlightthickness=0)
+
+    plane_seat1_entry.place(
+        x=83, y=305,
+        width=187,
+        height=35)
+
+    plane_seat1_entry_img = PhotoImage(file=f"img/Plane/img_textBox4.png")
+    plane_seat1_entry_bg = plane_canvas.create_image(
+        405.5, 323.5,
+        image=plane_seat1_entry_img)
+
+    plane_seat1_entry = Entry(
+        plane_window,
+        bd=0,
+        bg="#d9d9d9",
+        highlightthickness=0)
+
+    plane_seat1_entry.place(
+        x=312, y=305,
+        width=187,
+        height=35)
+
+    plane_manu_entry_img = PhotoImage(file=f"img/Plane/img_textBox5.png")
+    plane_manu_entry_bg = plane_canvas.create_image(
+        290.0, 401.5,
+        image=plane_manu_entry_img)
+
+    plane_manu_entry = Entry(
+        plane_window,
+        bd=0,
+        bg="#d9d9d9",
+        highlightthickness=0)
+
+    plane_manu_entry.place(
+        x=82, y=383,
+        width=416,
+        height=35)
+
+
+    global plane_img0
+    plane_img0 = PhotoImage(file=f"img/Plane/img0.png")
+    plane_b0 = Button(
+        plane_window,
+        image=plane_img0,
+        borderwidth=0,
+        highlightthickness=0,
+        command=plane_submit,
+        relief="flat")
+
+    plane_b0.place(
+        x=144, y=452,
+        width=293,
+        height=62)
+
+
+    global plane_img1
+    plane_img1 = PhotoImage(file=f"img/Plane/img1.png")
+    plane_b1 = Button(
+        plane_window,
+        image=plane_img1,
+        borderwidth=0,
+        highlightthickness=0,
+        command=plane_show,
+        relief="flat")
+
+    plane_b1.place(
+        x=144, y=530,
+        width=293,
+        height=62)
+
+    global plane_edit_entry
+    plane_edit_entry_img = PhotoImage(file=f"img/Plane/img_textBox6.png")
+    plane_edit_entry_bg = plane_canvas.create_image(
+        290.0, 674.5,
+        image=plane_edit_entry_img)
+
+    plane_edit_entry = Entry(
+        plane_window,
+        bd=0,
+        bg="#d9d9d9",
+        highlightthickness=0)
+
+    plane_edit_entry.place(
+        x=82, y=656,
+        width=416,
+        height=35)
+
+    global plane_img2
+    plane_img2 = PhotoImage(file=f"img/Plane/img2.png")
+    plane_b2 = Button(
+        plane_window,
+        image=plane_img2,
+        borderwidth=0,
+        highlightthickness=0,
+        command=edit,
+        relief="flat")
+
+    plane_b2.place(
+        x = 83, y = 741,
+        width=187,
+        height=62)
+
+    global plane_img3
+    plane_img3 = PhotoImage(file=f"img/Plane/img3.png")
+    plane_b3 = Button(
+        plane_window,
+        image=plane_img3,
+        borderwidth=0,
+        highlightthickness=0,
+        command=plane_delete,
+        relief="flat")
+
+    plane_b3.place(
+        x=308, y=741,
+        width=187,
+        height=62)
+
+
 img0 = PhotoImage(file = f"img/img0.png")
 b0 = Button(
     image = img0,
     borderwidth = 0,
     highlightthickness = 0,
-    command = btn_clicked,
+    command = plane_management_click,
     relief = "flat")
 
 b0.place(
@@ -298,7 +445,7 @@ b1 = Button(
     image = img1,
     borderwidth = 0,
     highlightthickness = 0,
-    command = btn_clicked,
+    command = plane_management_click,
     relief = "flat")
 
 b1.place(
@@ -311,7 +458,7 @@ b2 = Button(
     image = img2,
     borderwidth = 0,
     highlightthickness = 0,
-    command = btn_clicked,
+    command = plane_management_click,
     relief = "flat")
 
 b2.place(
@@ -324,7 +471,7 @@ b3 = Button(
     image = img3,
     borderwidth = 0,
     highlightthickness = 0,
-    command = btn_clicked,
+    command = plane_management_click,
     relief = "flat")
 
 b3.place(
@@ -337,7 +484,7 @@ b4 = Button(
     image = img4,
     borderwidth = 0,
     highlightthickness = 0,
-    command = btn_clicked,
+    command = plane_management_click,
     relief = "flat")
 
 b4.place(
